805877bd6cfa45a2b2b26c192e7912eb76837884
Matt Shull <derekshull@gmail.com>
2022-03-22 22:07:25 -0500

    mess up

--- problems/1_range.js
+++ problems/1_range.js
@@ -9,13 +9,7 @@ range(4,2) => []
 *******************************************************************************/
 
 function range(start, end) {
-  var array = [];
 
-  for(var i = start; i <= end; i += 1) {
-    array.push(i);
-  }
-
-  return array;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*************************/
